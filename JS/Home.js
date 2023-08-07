e834c9eeb5375bcfe9c9498809ea6bbac63ebf16
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-08-08 00:48:22 +0530

    footer responsive and menu structure done

--- JS/Home.js
+++ JS/Home.js
@@ -2,3 +2,7 @@ window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function handdleMenuClick(){
+    window.location.href = 'Menu.html' ;
+}
